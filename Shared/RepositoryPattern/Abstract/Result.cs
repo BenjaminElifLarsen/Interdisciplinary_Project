aa5b3942d5f90b82ca37ad0d8c95bfff9b32a7bd
Benjamin <beela93@hotmail.com>
2023-01-24 09:39:13 +0100

    cleaned up an attempt

--- Shared/RepositoryPattern/Abstract/Result.cs
+++ Shared/RepositoryPattern/Abstract/Result.cs
@@ -15,23 +15,3 @@ public abstract class Result : Result<object>
 {
 	public override object Data => default;
 }
-
-public abstract class ErrorResult<T> : Result<T>
-{
-	private readonly string[] _errors;
-    public override T Data => default;
-    public override string[] Errors { get => _errors; }
-
-	public ErrorResult(params string[] errors)
-	{
-		_errors = errors;
-	}
-}
-
-public abstract class ErrorResult : ErrorResult<object>
-{
-	public ErrorResult(params string[] errors) : base(errors)
-	{
-
-	}
-}
